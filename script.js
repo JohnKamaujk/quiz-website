7bd20e1085aa457d468abe4f89726aea6d4a3eb3
JohnKamaujk <johnjkkamau@gmail.com>
2022-05-14 11:50:51 +0300

    slightly adjusted the UI of the website

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let answers =["script","function myfunction()","myFunction()","if(i!=5)","while(i<=10)","for(i=0;i<=5;i++)",'["red","blue","green"]',"Math.round(7.25)","onclick","true"]
+var heading = document.getElementById("quiz")
+
+
+console.log(heading.textContent)
+
+
+
+
 
 
