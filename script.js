a5bffcfdbba248b0cc572ac8585fe4f02e75e036
JohnKamaujk <johnjkkamau@gmail.com>
2022-05-14 14:59:17 +0300

    function addded  all sections styled & completed

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-var heading = document.getElementById("quiz")
+// function to check if checked radio value is equal to correct answer
+function check() {
+  var score = 0;
 
+  if (document.getElementById("answer1").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer2").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer3").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer4").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer5").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer6").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer7").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer8").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer9").checked) {
+    score += 10;
+  }
+  if (document.getElementById("answer10").checked) {
+    score += 10;
+  }
 
-console.log(heading.textContent)
-
-
-
-
-
-
+  var writing = document.getElementById("demo");
+  writing.textContent = score;
+}
