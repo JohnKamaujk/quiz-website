1067083136350d0b2c7f8c398a81fb277052dca7
JohnKamaujk <johnjkkamau@gmail.com>
2022-05-14 20:35:15 +0300

    main function for incrementing marks added in js

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-// function to check if checked radio value is equal to correct answer
-function check() {
-  var score = 0;
+// starting marks is 0 as we increment with 10 for correct every answr
+let marks = 0;
 
+// create variables to store instance of form quiz & submitt button
+let quiz = document.getElementById("quiz");
+let btn = document.getElementById("btn");
+
+// this function verifies if correct answer is checked
+function verify() {
   if (document.getElementById("answer1").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer2").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer3").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer4").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer5").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer6").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer7").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer8").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer9").checked) {
-    score += 10;
+    marks += 10;
   }
   if (document.getElementById("answer10").checked) {
-    score += 10;
+    marks += 10;
   }
 
-  var writing = document.getElementById("demo");
-  writing.textContent = score;
+  // Hide the quiz form and submitt button to display the results
+  quiz.style.display = "none";
+  btn.style.display = "none";
+
+  let totalMarks = document.getElementById("demo");
+  totalMarks.textContent = marks;
+}
+function redirect() {
+  window.location = "results.html";
 }
